f1c8290c8e0bf304838cc0b6801bc5c7bc753b31
phutri27 <triphu27@gmail.com>
2026-06-15 14:38:35 +0700

    fix: auth.service

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,12 +26,14 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
       "@typescript-eslint/explicit-function-return-type": "error",
       "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,31 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { UsersService } from '../users/users.service.js';
-import { JwtService } from '@nestjs/jwt';
+import { Injectable } from '@nestjs/common'
+import { UsersService } from '../users/users.service'
+import { JwtService } from '@nestjs/jwt'
 import bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usersService: UsersService,
-        private jwtService: JwtService
-    ) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-    async validateUser(email: string, pass: string): Promise<any> {
-        const user = await this.usersService.findOne({email});
-        if (user){
-            const hashedPassword = await bcrypt.compare(pass, user.password); 
-            if (hashedPassword) {
-                const { password, ...result } = user;
-                return result;
-            }
-        }
-        return null;
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne({ email })
+    if (user) {
+      const hashedPassword = await bcrypt.compare(pass, user.password)
+      if (hashedPassword) {
+        const { password, ...result } = user
+        return result
+      }
     }
+    return null
+  }
 
-    async login(user: any){
-        const payload = { username: user.email, sub: user.id, role: user.role };
-        return {
-            access_token: this.jwtService.sign(payload),
-        };
+  login(user: any): { access_token: string } {
+    const payload = { username: user.email, sub: user.id, role: user.role }
+    return {
+      access_token: this.jwtService.sign(payload),
     }
+  }
 }
